9d8365d5ce3e7846166d3d2867f8169756f24965
André <javiandre2007@gmail.com>
2025-07-29 19:20:53 -0600

    agregar opciones y validar codigo

--- actividad10.py
+++ actividad10.py
@@ -3,28 +3,42 @@ print("-"*8 + "BIENVENIDO AL INVENTARIO DE PRODUCTOS" + "-"*8)
 quantity = int(input("Ingrese cuantos productos desea ingresar: "))
 for i in range(quantity):
     print(f"Producto #{i+1}: ")
-    code = input("Ingrese el codigo de producto(s) a ingresar: ")
-    name = input("Ingrese el nombre de producto(s) a ingresar: ")
-    category = input("Ingrese la categoria de producto(s) a ingresar: ")
-    size = input("Ingrese la talla de producto(s) a ingresar: ")
-    unity = input("Ingrese el precio unitario de producto(s) a ingresar: ")
-    stock = input("Ingrese el stock de producto(s) a ingresar: ")
-    products[code] = {
-        "code": code,
-        "name": name,
-        "category": category,
-        "size": size,
-        "unity": unity,
-        "stock": stock,
-    }
+    while True:
+        code = input("Ingrese el codigo de producto(s) a ingresar: ")
+        if code in products:
+            print("Este codigo ya existe")
+        else:
+            break
+        name = input("Ingrese el nombre de producto(s) a ingresar: ")
+        category = input("Ingrese la categoria de producto(s) a ingresar: ")
+        size = input("Ingrese la talla de producto(s) a ingresar: ")
+        unity = input("Ingrese el precio unitario de producto(s) a ingresar: ")
+        stock = input("Ingrese el stock de producto(s) a ingresar: ")
+        products[code] = {
+            "code": code,
+            "name": name,
+            "category": category,
+            "size": size,
+            "unity": unity,
+            "stock": stock,
+        }
 print("lISTA DE PRODUCTOS\n")
-for code in products:
+for code, dates in products.items():
     print(f"Producto #{code}: ")
-    code = products[code]["code"]
-    name = products[code]["name"]
-    category = products[code]["category"]
-    size = products[code]["size"]
-    unity = products[code]["unity"]
-    stock = products[code]["stock"]
+    print(f" Nombre: {dates["name"]}")
+    print(f" categoria: {dates["category"]}")
+    print(f" talla: {dates["size"]}")
+    print(f" precio unitario: Q{dates["unity"]} quetzales")
+    print(f" stock: {dates["stock"]} productos")
 print("-" * 8 + "Bienvenido a la busqueda de productos\n" + "-" * 8)
 search = int(input("Ingrese el codigo del producto que desea ingresar: "))
+if search in products:
+    print("¡Producto encontrado!")
+    print(f"Codigo del producto #{search}: {products['code']}")
+    print(f" Nombre del producto #{search}: {products['name']}")
+    print(f"Categoria del producto #{search}: {products['category']}")
+    print(f"Talla del producto #{search}: {products['size']}")
+    print(f"precio unitario del producto #{search}: Q{products['unity']}")
+    print(f"Quedan: {search}: #{products['stock']} productos")
+else:
+    print(f"Codigo del producto invalido")
