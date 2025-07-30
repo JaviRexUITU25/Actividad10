fb17f0e3454db9d43916e700db9ab3ff5bb9ccdf
André <javiandre2007@gmail.com>
2025-07-29 18:52:22 -0600

    Lista de productos y opcion de busqueda de productos

--- actividad10.py
+++ actividad10.py
@@ -9,4 +9,22 @@ for i in range(quantity):
     size = input("Ingrese la talla de producto(s) a ingresar: ")
     unity = input("Ingrese el precio unitario de producto(s) a ingresar: ")
     stock = input("Ingrese el stock de producto(s) a ingresar: ")
-
+    products[code] = {
+        "code": code,
+        "name": name,
+        "category": category,
+        "size": size,
+        "unity": unity,
+        "stock": stock,
+    }
+print("lISTA DE PRODUCTOS\n")
+for code in products:
+    print(f"Producto #{code}: ")
+    code = products[code]["code"]
+    name = products[code]["name"]
+    category = products[code]["category"]
+    size = products[code]["size"]
+    unity = products[code]["unity"]
+    stock = products[code]["stock"]
+print("-" * 8 + "Bienvenido a la busqueda de productos\n" + "-" * 8)
+search = int(input("Ingrese el codigo del producto que desea ingresar: "))
